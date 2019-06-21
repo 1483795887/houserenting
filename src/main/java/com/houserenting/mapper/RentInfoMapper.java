ab5909eb1b1e4538e6659a268963300c529710c7
W83220719 <1483795887@qq.com>
2019-06-21 09:12:39 +0800

    replace all costumer to customer

--- src/main/java/com/houserenting/mapper/RentInfoMapper.java
+++ src/main/java/com/houserenting/mapper/RentInfoMapper.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RentInfoMapper {
     RentInfo sel(int rid);
+    void add(RentInfo rentInfo);
 }
--- src/test/java/com/houserenting/mapper/RentInfoMapperTest.java
+++ src/test/java/com/houserenting/mapper/RentInfoMapperTest.java
@@ -1,11 +1,15 @@
 package com.houserenting.mapper;
 
+import com.houserenting.entity.Customer;
+import com.houserenting.entity.RentInfo;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,8 +17,59 @@ public class RentInfoMapperTest {
     @Autowired
     RentInfoMapper rentInfoMapper;
 
+    @Autowired
+    CustomerMapper customerMapper;
+
+    private Customer customer;
+    private RentInfo rentInfo;
+
+    @Before
+    public void setUp() throws Exception {
+        customer.setUsername("username");
+        customer.setPassword("password");
+        customer.setTel("18696104532");
+        customer.setSex(Customer.MALE);
+        customer.setRealname("realName");
+        customer.setAddress("wuhan");
+        customer.setAge(24);
+        customerMapper.add(customer);
+
+
+    }
+
     @Test
+    @Transactional
     public void selRandomWhenNotDatabaseIsEmpty() {
         assertNull(rentInfoMapper.sel(100));
     }
+
+    @Test
+    @Transactional
+    public void whenAddedTheLayoutIsTheSame(){
+        RentInfo rentInfo = new RentInfo();
+        rentInfo.setLayout("三室两厅");
+        rentInfo.setCustomer(customer);
+
+        rentInfoMapper.add(rentInfo);
+
+        RentInfo rentInfo1 = rentInfoMapper.sel(rentInfo.getRid());
+
+        assertEquals(rentInfo1.getLayout(), rentInfo.getLayout());
+    }
+
+    @Test
+    @Transactional
+    public void whenAddedUsernameOfTheCustomerIsSame() {
+        customerMapper.add(customer);
+        RentInfo rentInfo = new RentInfo();
+        rentInfo.setCustomer(customer);
+
+        rentInfoMapper.add(rentInfo);
+
+        RentInfo rentInfo1 = rentInfoMapper.sel(rentInfo.getRid());
+
+        assertEquals(rentInfo.getCustomer().getUsername(),rentInfo1.getCustomer().getUsername());
+    }
+
+
 }
